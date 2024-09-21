971ede25c97ef5fd04763d21c0b2df52156591d5
Noushika <noushika.27@gmail.com>
2024-09-21 10:12:32 +0530

    1stfile added

---
+++ recurrsion.js
@@ -0,0 +1,13 @@
+function reversee(str) {
+  let arr = str.split("");
+
+  console.log(arr);
+  let arr1;
+  for(i=arr.length-1,i=>0,--i){
+    arr1= arr[i].push();
+
+  }
+  console.log(arr1);
+}
+
+reversee("hell");
